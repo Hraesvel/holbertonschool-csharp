f95686f88acdd608cd65b196713ead5242ceba81
Martin smith <msmith3dart@icloud.com>
2020-01-27 15:01:39 -0800

    use C format

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,6 +6,6 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:0.00%}", percent);	
-		Console.WriteLine("Currency: ${0:N2}", currency);
+		Console.WriteLine("Currency: {0:C}", currency);
 	}
 }
