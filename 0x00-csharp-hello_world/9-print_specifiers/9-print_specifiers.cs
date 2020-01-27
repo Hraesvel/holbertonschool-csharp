db86e237c9d819e8c3cc18bebb62772f97857861
Martin smith <msmith3dart@icloud.com>
2020-01-27 14:04:05 -0800

    trying another solution

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:0.00%}", percent);
-		Console.WriteLine(String.Format("Currency: ${0:#,0.00}", currency));
+		Console.WriteLine("Currency: ${0}", currency.ToString("#,##0.00"));
 	}
 }
