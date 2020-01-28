676e99c7525488b591af37c167ea79eb0ef50096
Martin <msmith3dart@icloud.com>
2020-01-28 14:06:28 -0800

    fixed typo

--- 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -12,11 +12,11 @@ class Program
         }
         else if (number < 0)
         {
-            Console.WriteLine("{0} is negitive", number);
+            Console.WriteLine("{0} is negative", number);
         }
         else
         {
-            Console.WriteLine("{0} is zero", number);
+            Console.WriteLine("is zero");
         }
     }
 }
