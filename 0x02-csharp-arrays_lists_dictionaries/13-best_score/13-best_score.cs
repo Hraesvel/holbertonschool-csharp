ea31bc086f800ae79995e88e98f2daca4d5ca457
Martin <msmith3dart@icloud.com>
2020-01-30 16:28:13 -0800

    changed default score value.

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -2,16 +2,14 @@ using System.Collections.Generic;
 
 class Dictionary {
     public static string BestScore(Dictionary<string, int> myList){
-        if (myList == null)
-            return "None";
         string best = "None";
-        int score = 0;
+        int score = -1;
 
         foreach((string k, int v) in myList){
             if (v > score)
                 (best, score) = (k,v);
         }
 
-        return score > 0 ? best : "None";
+        return best;
     }
 }
