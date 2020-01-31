f02bd4be0a696b5df058f09b21ee1fe73b014c30
Martin <msmith3dart@icloud.com>
2020-01-30 16:21:19 -0800

    modify conditions.

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 
 class Dictionary {
     public static string BestScore(Dictionary<string, int> myList){
+        // if (myList == null)
+        //     return "None";
         string best = "None";
         int score = 0;
 
@@ -10,6 +12,6 @@ class Dictionary {
                 (best, score) = (k,v);
         }
 
-        return best;
+        return score > 0 ? best : "None";
     }
 }
