167852f07cc5d3f2a962327d72a18d69b415eb1c
Martin <msmith3dart@icloud.com>
2020-01-30 16:18:26 -0800

    set default string to expected value.

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 class Dictionary {
     public static string BestScore(Dictionary<string, int> myList){
-        string best = "martin";
+        string best = "None";
         int score = 0;
 
         foreach((string k, int v) in myList){
