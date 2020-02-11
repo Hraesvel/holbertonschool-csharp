c0e8d13c57652556af274929f4abd72f6b78716f
Martin <msmith3dart@icloud.com>
2020-02-10 20:06:37 -0800

    fixed a case for LLists with a single node.
    fixed issue with single node LList with a value greater then one supplied.

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/main.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/main.cs
@@ -11,11 +11,11 @@ class Program
 
         myLList.AddLast(1);
         myLList.AddLast(4);
-        myLList.AddLast(9);
-        myLList.AddLast(16);
-        myLList.AddLast(25);
-        myLList.AddLast(36);
-        myLList.AddLast(49);
+        // myLList.AddLast(9);
+        // myLList.AddLast(16);
+        // myLList.AddLast(25);
+        // myLList.AddLast(36);
+        // myLList.AddLast(49);
 
         current = myLList.First;
         while (current != null)
@@ -25,7 +25,7 @@ class Program
         }
 
         Console.WriteLine("------------------");
-        LList.Insert(myLList, 21);
+        LList.Insert(myLList, 2);
 
         current = myLList.First;
         while (current != null)
