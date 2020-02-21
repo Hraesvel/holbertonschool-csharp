814b13737fcaf839d1ae34186bad31ee3f7b5d55
Martin <msmith3dart@icloud.com>
2020-02-21 13:09:40 -0800

    update

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -9,13 +9,13 @@ class Obj {
     /// </summary>
     /// <param myObj="object to inspect"></param>
     public static void Print(object myObj) {
-        TypeInfo t = myObj.GetType ().GetTypeInfo ();
-        var prop = t.DeclaredProperties;
-        var method = t.DeclaredMethods;
-        WriteLine ("{0} Properties", myObj.GetType ().Name);
+        var t = myObj.GetType();
+        var prop = t.GetTypeInfo ().DeclaredProperties;
+        var method = t.GetMethods();
+        WriteLine ("{0} Properties", myObj.GetType().Name);
         foreach (var i in prop)
             WriteLine (i.Name);
-        WriteLine ("{0} Methods", myObj.GetType ().Name);
+        WriteLine ("{0} Methods", myObj.GetType().Name);
         foreach (var i in method)
             if (i.IsPublic)
                 WriteLine (i.Name);
