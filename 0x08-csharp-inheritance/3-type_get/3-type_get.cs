fa3f5432d2842181d88b0d2494a293eea9252f60
Martin <msmith3dart@icloud.com>
2020-02-21 13:23:59 -0800

    fix printing properties

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -8,14 +8,17 @@ class Obj {
     ///  prints the names of the available properties and methods of an object.
     /// </summary>
     /// <param myObj="object to inspect"></param>
-    public static void Print(object myObj) {
-        var t = myObj.GetType();
-        var prop = t.GetTypeInfo ().DeclaredProperties;
-        var method = t.GetMethods();
-        WriteLine ("{0} Properties", myObj.GetType().Name);
+    public static void Print (object myObj) {
+        var t = myObj.GetType ();
+
+        var prop = t.GetProperties ();
+        var method = t.GetMethods ();
+        
+        WriteLine ("{0} Properties", myObj.GetType ().Name);
         foreach (var i in prop)
-            WriteLine (i.Name);
-        WriteLine ("{0} Methods", myObj.GetType().Name);
+                WriteLine (i.Name);
+
+        WriteLine ("{0} Methods", myObj.GetType ().Name);
         foreach (var i in method)
             if (i.IsPublic)
                 WriteLine (i.Name);
