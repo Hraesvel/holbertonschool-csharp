6cdcecf37627e2b17cff7e42e7c86af5b8ce92f7
Martin Smith <mcodesmith@gmail.com>
2020-05-12 18:09:41 -0700

    checker stuff

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,6 +1,20 @@
-﻿abstract class Base {
+﻿using System;
+
+abstract class Base {
+    /// <summary>
+    /// public name of object
+    /// </summary>
     public string name;
 
+    protected Base(string name)
+    {
+        this.name = name;
+    }
+
+    /// <summary>
+    /// return a message
+    /// </summary>
+    /// <returns>string</returns>
     public override string ToString(){
         return string.Format("{0} is a {1}", name, this.GetType().Name);
     }
--- 0x0B-csharp-interfaces/1-user_interface/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/1-user_interface/0-abstract_thinking.cs
@@ -1,6 +1,20 @@
+using System;
+
 abstract class Base {
+    /// <summary>
+    /// public name of object
+    /// </summary>
     public string name;
 
+    protected Base(string name)
+    {
+        this.name = name;
+    }
+
+    /// <summary>
+    /// return a message
+    /// </summary>
+    /// <returns>string</returns>
     public override string ToString(){
         return string.Format("{0} is a {1}", name, this.GetType().Name);
     }
