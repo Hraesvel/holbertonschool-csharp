1a2a6f48f1f76c0c84ea61e2076382077fa61756
Martin Smith <mcodesmith@gmail.com>
2020-05-13 23:04:36 -0700

    update

--- 0x0C-csharp-delegates_events/4-check_yourself/4-check_yourself.cs
+++ 0x0C-csharp-delegates_events/4-check_yourself/4-check_yourself.cs
@@ -68,7 +68,7 @@ public class Player
     /// </summary>
     public void PrintHealth()
     {
-        Console.WriteLine($"{name} has {hp} / {maxHp} health.");
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
     }
 
 
--- 0x0C-csharp-delegates_events/5-eventful/5-eventful.cs
+++ 0x0C-csharp-delegates_events/5-eventful/5-eventful.cs
@@ -67,7 +67,7 @@ public class Player
     /// </summary>
     public void PrintHealth()
     {
-        Console.WriteLine($"{name} has {hp} / {maxHp} health.");
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
     }
 
 
