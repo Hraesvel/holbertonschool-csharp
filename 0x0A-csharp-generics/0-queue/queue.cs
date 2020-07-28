ad3e45aa090ad8fdb6cb895df13e2e14aa4ed21c
Martin Smith <mcodesmith@gmail.com>
2020-07-28 13:48:59 -0700

    xml doc

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -1,5 +1,15 @@
-class Queue<T> {
+using System;
+
+/// <summary>
+/// My implementation of Queue type
+/// </summary>
+/// <typeparam name="T">base type</typeparam>
+public class Queue<T> {
+    /// <summary>
+    /// check Queue base type
+    /// </summary>
+    /// <returns>string rep of base type</returns>
     public Type CheckType () {
-        return typeof(T);
+        return typeof (T);
     }
 }
